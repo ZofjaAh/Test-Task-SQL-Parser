a049b9223dec5a62072e0f420f7aaaf3c551c509
Sofya Aheyeva <zozolulu1894@gmail.com>
2024-06-17 14:05:13 +0300

    fourth commit

--- src/main/java/com/aheyeva/sql/parser/Application.java
+++ src/main/java/com/aheyeva/sql/parser/Application.java
@@ -13,25 +13,31 @@ public class Application {
                 "SELECT name, email, age FROM users",
                 "SELECT * FROM users WHERE age > 30",
                 "SELECT * FROM users WHERE age > 30 AND email LIKE '%@example.com'",
-                "SELECT users.name, orders.order_date, orders.total \n" +
-                        "FROM users \n" +
-                        "INNER JOIN orders ON users.id = orders.user_id",
-                "SELECT users.name, orders.order_date, orders.total\n" +
-                        "FROM users\n" +
-                        "LEFT JOIN orders ON users.id = orders.user_id",
-                "SELECT category, COUNT(*) as total_products\n" +
-                        "FROM products\n" +
-                        "GROUP BY category\n" +
-                        "HAVING total_products > 10",
-                "SELECT * FROM users\n" +
-                        "ORDER BY name ASC, age DESC",
-                "SELECT * FROM users\n" +
-                        "LIMIT 10 OFFSET 20",
-                "SELECT *\n" +
-                        "FROM (\n" +
-                        "  SELECT * FROM users WHERE age > 30\n" +
-                        ") as older_users\n" +
-                        "WHERE older_users.email LIKE '%@example.com'"
+                """
+                        SELECT users.name, orders.order_date, orders.total
+                        FROM users
+                        INNER JOIN orders ON users.id = orders.user_id""",
+                """
+                        SELECT users.name, orders.order_date, orders.total
+                        FROM users
+                        LEFT JOIN orders ON users.id = orders.user_id""",
+                """
+                        SELECT category, COUNT(*) as total_products
+                        FROM products
+                        GROUP BY category
+                        HAVING total_products > 10""",
+                """
+                        SELECT * FROM users
+                        "ORDER BY name ASC, age DESC""",
+                """
+                        SELECT * FROM users
+                        "LIMIT 10 OFFSET 20""",
+                """
+                        SELECT *
+                        FROM (
+                          SELECT * FROM users WHERE age > 30
+                        ) as older_users
+                        WHERE older_users.email LIKE '%@example.com'"""
         );
         SqlParser parser = new SqlParser();
         listCommands.stream()
